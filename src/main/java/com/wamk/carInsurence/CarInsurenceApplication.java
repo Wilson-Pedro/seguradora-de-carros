1c05f173d9c3112f176c10c757233a089e72d8b2
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-04-14 16:50:51 -0300

    first commit

---
+++ src/main/java/com/wamk/carInsurence/CarInsurenceApplication.java
@@ -0,0 +1,13 @@
+package com.wamk.carInsurence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInsurenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInsurenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wamk/carInsurence/CarInsurenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wamk.carInsurence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInsurenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
